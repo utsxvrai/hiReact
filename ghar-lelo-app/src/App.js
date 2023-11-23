c63f4ff032194cc9db9a61cc92fc1e4895a3391c
Utsav Rai <utsavrai2115@gmail.com>
2023-11-23 09:28:59 +0530

    added pages and routes

--- ghar-lelo-app/src/App.js
+++ ghar-lelo-app/src/App.js
@@ -1,8 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignOff from "./pages/SignOff";
+import SignIn from "./pages/Signin";
+import ForgotPass from "./pages/ForgotPass";
 function App() {
   return (
     < >
-      <h1 className="text-2xl bg-red-">Ghar Lelo</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/offers" element={<h1>Offers</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+          <Route path="/sign-off" element={<h1>Sign Off</h1>} />
+          <Route path="/sign-in" element={<h1>Sign In</h1>} />
+          <Route path="/forgot-password" element={<h1>Forgot Pass</h1>} />
+
+        </Routes>
+      </Router>
     </>
   );
 }
