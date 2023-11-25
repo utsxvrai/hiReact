312919a5393ddd79ee755f96c079b90c06c866db
Utsav Rai <utsavrai2115@gmail.com>
2023-11-25 08:55:22 +0530

    navigation through pages

--- ghar-lelo-app/src/components/Header.jsx
+++ ghar-lelo-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useLocation} from 'react-router-dom'
+import {useLocation , useNavigate} from 'react-router-dom'
 
 
 export default function Header() {
@@ -11,17 +11,29 @@ export default function Header() {
     }
   }
 
+  const navigate = useNavigate()
+  function navigateToSignIn() {
+    navigate('/sign-in')
+  }
+  function navigateToHome() {
+    navigate('/')
+  }
+  function navigateToOffers() {
+    navigate('/offers')
+  }
+
+
   return (
-    <div className='bg-grey border-b shadow-sm sticky top-0 z-50'>
-        <header className='flex justify-between items-center px-3 py-3 max-w-6xl mx-auto '>
+    <div className='bg-gray-100 border-b shadow-sm sticky top-0 z-50'>
+        <header className='flex justify-between items-center px-3  max-w-6xl mx-auto '>
             <div>
-                <img src="/logo192.png" alt="logo" className="h-10 cursor-pointer" />
+                <img src="/logo192.png" alt="logo" className="h-10 cursor-pointer" onClick={()=>navigateToHome()} />
             </div>
             <div>
               <ul className='flex space-x-10'>
-                <li className={`text-l font-semibold text-green-800 border-b-[3px] border-b-transparent ${pathMathRoute('/') && "text-black border-b-green-700"}`}>Home</li>
-                <li className={`text-l font-semibold text-green-800 border-b-[3px] border-b-transparent ${pathMathRoute('/offers') && "text-black border-b-green-700"}`}>Offers</li>
-                <li className={`text-l font-semibold text-green-800 border-b-[3px] border-b-transparent ${pathMathRoute('/sign-in') && "text-black border-b-green-700"}`}>Sign In</li>
+                <li className={`text-l cursor-pointer py-4 font-semibold text-green-800 border-b-[3px] border-b-transparent ${pathMathRoute('/') && "text-black border-b-green-700"}`} onClick={()=>navigateToHome()}>Home</li>
+                <li className={`text-l cursor-pointer py-4 font-semibold text-green-800 border-b-[3px] border-b-transparent ${pathMathRoute('/offers') && "text-black border-b-green-700"}`} onClick={()=> navigateToOffers()}>Offers</li>
+                <li className={`text-l cursor-pointer py-4 font-semibold text-green-800 border-b-[3px] border-b-transparent ${pathMathRoute('/sign-in') && "text-black border-b-green-700"}`} onClick={()=> navigateToSignIn()}>Sign In</li>
               </ul>
             </div>
         </header>
