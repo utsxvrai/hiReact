9510ee0336752168fc562ebf89b052948d29d4de
Utsav Rai <utsavrai2115@gmail.com>
2023-11-29 11:48:48 +0530

    implemented the Oauth page!!

--- ghar-lelo-app/src/pages/Signin.jsx
+++ ghar-lelo-app/src/pages/Signin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -22,7 +23,7 @@ export default function SignIn() {
 
   return (
     <section>
-      <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
+      <h1 className='text-3xl text-center mt-6 font-bold'>SIGN IN</h1>
       <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
         <div className='md:w-[67%] lg:w-[50%] mb-12 md-mb-6'>
           <img
@@ -66,22 +67,23 @@ export default function SignIn() {
               )}
                 </div>
                 <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
-                  <p className='mb-6' > Don't have account?
-                  <Link to='/signup' className='text-red-600 hover:text-red-700 ml-2 transition duration-200 ease-in-out'>Register</Link>
+                  <p className='mb-6 text-sm' > Don't have account?
+                  <Link to='/signup' className='text-red-600 text-sm hover:text-red-700 ml-2 transition duration-200 ease-in-out'>Register</Link>
                   </p>
                   <p>
-                    <Link to='/forgotpassword' className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>Forgot Password?</Link>
+                    <Link to='/forgotpassword' className='text-blue-600 text-sm hover:text-blue-800 transition duration-200 ease-in-out'>Forgot Password?</Link>
                   </p>
                 </div>
-            </form>
-            <button className='bg-green-600 hover:bg-green-700 transition duration-200 ease-in-out text-white text-xl font-semibold px-8 py-2 rounded-md w-full shadow-sm hover:shadow-lg'>SIGN IN
+                <button className='bg-green-600 hover:bg-green-700 transition duration-200 ease-in-out text-white text-sm font-semibold px-8 py-2 rounded-md w-full shadow-sm hover:shadow-lg'>SIGN IN
             </button>
             <div className=' flex my-4 items-center 
             before:border-t before:flex-1 before:border-green-300 after:border-t after:flex-1 after:border-green-300'>
-              <p className='text-center font-bold mx-4'>
+              <p className='text-center font-semibold mx-4'>
               OR
               </p>
             </div>
+            <OAuth />
+            </form> 
         </div>  
       </div>
     </section>
